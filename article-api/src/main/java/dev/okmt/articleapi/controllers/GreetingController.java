891e1b3320842d0d7bde6a29da9d9890579ba300
岡本祐樹 <okmt@okmt.local>
2023-03-22 23:57:13 +0900

    minor update

---
+++ article-api/src/main/java/dev/okmt/articleapi/controllers/GreetingController.java
@@ -0,0 +1,17 @@
+package dev.okmt.articleapi.controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import dev.okmt.shared.models.HelloWorld;
+
+@RestController
+public class GreetingController {
+    @GetMapping("/hello-world")
+    public ResponseEntity<String> getHealth() {
+        HelloWorld helloWorld = new HelloWorld();
+        helloWorld.setGreeting("Hello World");
+        return ResponseEntity.ok(helloWorld.getGreeting());
+    }
+}
---
+++ shared/src/main/java/dev/okmt/shared/models/HelloWorld.java
@@ -0,0 +1,13 @@
+package dev.okmt.shared.models;
+
+public class HelloWorld {
+    private String greeting;
+
+    public String getGreeting() {
+        return greeting;
+    }
+
+    public void setGreeting(String greeting) {
+        this.greeting = greeting;
+    }
+}
